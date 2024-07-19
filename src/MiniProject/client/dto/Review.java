17261667a4ea4df917cb0c503847d2665697c090
13wjdgk <13wjdgk@naver.com>
2024-07-19 16:17:07 +0900

    feat : Review DTO

---
+++ src/MiniProject/client/dto/Review.java
@@ -0,0 +1,69 @@
+package MiniProject.client.dto;
+
+import java.time.LocalDateTime;
+
+public class Review {
+	private int reviewId;
+	private int storeId;
+	private int userId;
+	private int orderId;
+	private String content;
+	private int rating;
+	private LocalDateTime createdAt;
+
+	public int getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public int getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+}
